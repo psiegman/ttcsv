dcdc96a5f25683b138d285450e8448ba8c07b2d4
P. Siegmann <paul@siegmann.nl>
2020-10-04 10:50:27 +0200

    Remove commented-out code, have the BeanFactory return a stream of beans.

---
+++ src/test/java/nl/siegmann/ttcsv/bean/BeanFactoryTest.java
@@ -0,0 +1,136 @@
+package nl.siegmann.ttcsv.bean;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+import nl.siegmann.ttcsv.bean.converter.Converter;
+import nl.siegmann.ttcsv.csv.CsvConfig;
+import nl.siegmann.ttcsv.csv.CsvReader;
+import nl.siegmann.ttcsv.util.StringIteratorReader;
+import nl.siegmann.ttcsv.util.StringUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BeanFactoryTest {
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Person {
+        private long id;
+        private String name;
+        private Instant lastUpdate;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Fruit {
+        private String name;
+        private float price;
+    }
+
+    private BeanFactoryBuilder beanFactoryBuilder;
+
+    @BeforeEach
+    public void setUp() {
+        this.beanFactoryBuilder = new BeanFactoryBuilder();
+    }
+
+
+    @Test
+    public void test_two_persons_stream() throws Exception {
+        // given
+        Stream<List<String>> csvStream = createCsvStream(
+                "id|name|lastUpdate",
+                "1|alice|2020-10-26T10:15:30.00Z",
+                "2|bob|2020-10-26T11:15:30.00Z"
+        );
+        Function<List<String>, Stream<Person>> beanFactory = beanFactoryBuilder.createBeanFactory(Person.class);
+
+        // when
+        List<Person> persons = csvStream
+                .flatMap(beanFactory)
+                .collect(Collectors.toList());
+
+        // then
+        assertThat(persons).isEqualTo(Arrays.asList(
+                new Person(1, "alice", Instant.parse("2020-10-26T10:15:30.00Z")),
+                new Person(2, "bob", Instant.parse("2020-10-26T11:15:30.00Z"))
+        ));
+    }
+
+    /**
+     * Reads a floating point number with a ',' as a decimal separator.
+     */
+    private static class CustomFloatConverter implements Converter<Float> {
+        private DecimalFormat decimalFormat;
+
+        public CustomFloatConverter() {
+
+            DecimalFormatSymbols decimalFormatSymbols = new DecimalFormatSymbols();
+            decimalFormatSymbols.setDecimalSeparator(',');
+            decimalFormatSymbols.setGroupingSeparator(' ');
+
+            DecimalFormat decimalFormat = new DecimalFormat();
+            decimalFormat.setDecimalFormatSymbols(decimalFormatSymbols);
+
+            this.decimalFormat = decimalFormat;
+        }
+
+        @SneakyThrows
+        @Override
+        public Float apply(String s) {
+            if (StringUtils.isBlank(s)) {
+                return 0.0f;
+            }
+            return decimalFormat.parse(s).floatValue();
+        }
+    }
+
+    @DisplayName("Read CSV data to beans using a custom converter that uses ',' as a decimal separator")
+    @Test
+    public void test_custom_converter() throws Exception {
+        // given
+        Stream<List<String>> csvStream = createCsvStream(
+                "name|price",
+                "apple|1,25",
+                "pear|1,37"
+        );
+        BeanFactory<Fruit> beanFactory = beanFactoryBuilder.createBeanFactory(Fruit.class);
+        beanFactory.getConverterRegistry().registerConverter(Float.TYPE, new CustomFloatConverter());
+
+        // when
+        List<Fruit> fruits = csvStream
+                .flatMap(beanFactory)
+                .collect(Collectors.toList());
+
+        // then
+        assertThat(fruits).isEqualTo(Arrays.asList(
+                new Fruit("apple", 1.25f),
+                new Fruit("pear", 1.37f)
+        ));
+    }
+
+    Stream<List<String>> createCsvStream(String... csvData) {
+        return new CsvReader(new CsvConfig().withFieldSeparator('|'))
+                .apply(StringIteratorReader.of("\n", csvData));
+    }
+}
